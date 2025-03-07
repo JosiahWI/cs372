2210c889cb7b45d09045bb77370d50080a742dd7
Josiah VanderZee <jvanderzee@apache.org>
2025-03-07 12:51:54 -0600

    Read resistance and show it on the LCD

--- week06/lcd/lcd.ino
+++ week06/lcd/lcd.ino
@@ -1,16 +1,28 @@
-#include <Arduino.h>
+#include "types.h"
 
-#include <string_view>
+#include <Arduino.h>
 
 #define lcd Serial2
 
-using serial_t = decltype(lcd);
-
 static void wait_for_serial_ready(serial_t& serial);
+static long read_resistance();
+static void show_resistance(long r);
+
+namespace {
+  constexpr pin_t POT_SENSOR_PIN{16};
+  constexpr int       INPUT_MV{3300};
+  constexpr int       MAX_OUTPUT_MV{3000};
+  constexpr long long R1_MOHM{2200000};
+  constexpr long long POT_TOTAL_MOHM{10000000};
+  constexpr int       LCD_OHM_SYMBOL{0b11110100};
+  constexpr int       LCD_COMMAND_MODE{0xfe};
+  constexpr int       LCD_COMMAND_CLR{0x01};
+} // end anonymous namespace
 
 void
 setup()
 {
+  pinMode(POT_SENSOR_PIN, INPUT);
   Serial2.begin(9600);
   wait_for_serial_ready(lcd);
 }
@@ -24,9 +36,25 @@ wait_for_serial_ready(serial_t& serial)
 void
 loop()
 {
-  static int done_display = 0;
-  if (!done_display) {
-    lcd.write("JV");
-    done_display = 1;
-  }
+  show_resistance(read_resistance());
+}
+
+long
+read_resistance()
+{
+  int const  level{static_cast<int>(analogRead(POT_SENSOR_PIN))};
+  int const  mvolts{1000 * level * MAX_OUTPUT_MV / 1023};
+  long const mohms{static_cast<long>(
+    (mvolts * (POT_TOTAL_MOHM + R1_MOHM) - INPUT_MV * R1_MOHM) /
+    (INPUT_MV - mvolts))};
+  return mohms;
+}
+
+void
+show_resistance(long r)
+{
+  lcd.write(LCD_COMMAND_MODE);
+  lcd.write(LCD_COMMAND_CLR);
+  lcd.print(r);
+  lcd.write(LCD_OHM_SYMBOL);
 }
