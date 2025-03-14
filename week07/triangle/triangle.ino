ccd85140d45e5a57cd882ea8d49ad9bdb2758a4d
Josiah VanderZee <jvanderzee@apache.org>
2025-03-14 14:11:24 -0500

    Use constexpr array instead of vector

--- week07/triangle/triangle.ino
+++ week07/triangle/triangle.ino
@@ -3,15 +3,26 @@
 #include "DACOscillate.h"
 #include "types.h"
 
+#include <array>
 #include <cassert>
 #include <cstdint>
 #include <limits>
 #include <vector>
 
-static std::vector<DACChunk> chunk_range(int n);
+template <int N>
+constexpr std::array<DACChunk, N>
+chunk_range() {
+  static_assert(N <= std::numeric_limits<std::uint8_t>::max() + 1);
+
+  std::array<DACChunk, N> result;
+  for (int i{0}; i < N; ++i) {
+    result[i] = DACChunk{static_cast<std::uint8_t>(i)};
+  }
+  return result;
+}
 
 namespace {
-  auto voltage_line{chunk_range(256)};
+  constexpr auto voltage_line{chunk_range<256>()};
   auto g_DAC_pump{DACOscillate{voltage_line.begin(), voltage_line.end()}};
 } // end anonymous namespace
 
@@ -22,14 +33,3 @@ void loop() {
   g_DAC_pump.step();
 }
 
-std::vector<DACChunk>
-chunk_range(int n) {
-  assert(n <= std::numeric_limits<std::uint8_t>::max() + 1);
-
-  std::vector<DACChunk> result;
-  result.resize(n);
-  for (int i{0}; i < n; ++i) {
-    result[i] = DACChunk{static_cast<std::uint8_t>(i)};
-  }
-  return result;
-}
