9a1214d6e8e0085aae45e87c067ed98fb304bb85
Josiah VanderZee <jvanderzee@apache.org>
2025-04-04 13:49:28 -0500

    Use uniform initialization in setup

--- week09/sram/sram.ino
+++ week09/sram/sram.ino
@@ -7,7 +7,7 @@ setup()
 {
   int dataPins[] = {7, 8, 9, 10, 11, 23, 22, 21};
   int addrPins[] = {20, 19, 18, 17, 16, 15, 14, 13};
-  my_device      = new ParallelSRAM(dataPins, addrPins, 5, 4);
+  my_device      = new ParallelSRAM{dataPins, addrPins, 5, 4};
 
   Serial.begin(9600);
   while (!Serial)
