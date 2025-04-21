1ac2939abb11480103d2e365be1a284775e06aca
Josiah VanderZee <jvanderzee@apache.org>
2025-04-21 10:02:55 -0500

    Correct all bad data bits on EEPROM

--- week11/eeprom/eeprom.ino
+++ week11/eeprom/eeprom.ino
@@ -1,3 +1,4 @@
+#include "cs372ecc.h"
 #include "CS372EEPROM.h"
 
 #include <Arduino.h>
@@ -14,11 +15,21 @@ setup()
 void
 loop()
 {
-  static bool did_oneshot{false};
-  if (!did_oneshot) {
-    did_oneshot = true;
-    eeprom.write_eeprom(0x0, 0xff);
-    delay(10);
-    Serial.print(eeprom.read_eeprom(0x0));
+  static int next_addr{0xa};
+  static int num_corrected{0};
+  if (next_addr <= 0xff) {
+    std::uint8_t ah{eeprom.read_eeprom(next_addr)};
+    std::uint8_t al{eeprom.read_eeprom(next_addr)};
+    int          val{(ah << 8) | al};
+    int          corrected_val{check_hamming(val)};
+    if (val != corrected_val) {
+      ++num_corrected;
+      delay(5);
+      eeprom.write_eeprom(next_addr, corrected_val);
+    }
+    ++next_addr;
+  } else if (next_addr == 0x100) {
+    ++next_addr;
+    Serial.print(num_corrected);
   }
 }
