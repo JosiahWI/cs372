0df46591a91993185bb6b3ee0aa8d2f194aef138
Josiah VanderZee <jvanderzee@apache.org>
2025-04-21 10:08:09 -0500

    Fix address pairs

--- week11/eeprom/eeprom.ino
+++ week11/eeprom/eeprom.ino
@@ -15,19 +15,20 @@ setup()
 void
 loop()
 {
-  static int next_addr{0xa};
+  static int next_addr{0x0a};
   static int num_corrected{0};
   if (next_addr <= 0xff) {
     std::uint8_t ah{eeprom.read_eeprom(next_addr)};
-    std::uint8_t al{eeprom.read_eeprom(next_addr)};
+    std::uint8_t al{eeprom.read_eeprom(next_addr + 1)};
     int          val{(ah << 8) | al};
     int          corrected_val{check_hamming(val)};
     if (val != corrected_val) {
       ++num_corrected;
       delay(5);
-      eeprom.write_eeprom(next_addr, corrected_val);
+      eeprom.write_eeprom(next_addr, (corrected_val & 0xff00) >> 8);
+      eeprom.write_eeprom(next_addr + 1, (corrected_val & 0x00ff));
     }
-    ++next_addr;
+    next_addr += 2;
   } else if (next_addr == 0x100) {
     ++next_addr;
     Serial.print(num_corrected);
