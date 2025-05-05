a573ca55e0a017116b8c972da6aa7d4e297dd553
Josiah VanderZee <jvanderzee@apache.org>
2025-05-05 10:39:08 -0500

    Add stepper motor controller

---
+++ week13/stepper/stepper.ino
@@ -0,0 +1,65 @@
+#include "types.h"
+
+#include <Arduino.h>
+
+#include <csignal>
+
+static IntervalTimer* get_g_step_timer();
+static void           timer_handler();
+
+namespace {
+// In1, In2, In3, In4
+constexpr pin_t STEPPER_PINS[]{22, 21, 19, 20};
+
+using stepper_state_t = dig_t[4];
+constexpr stepper_state_t STATE_ORDER[]{
+    { LOW, HIGH,  LOW,  LOW},
+    { LOW,  LOW,  LOW, HIGH},
+    {HIGH,  LOW,  LOW,  LOW},
+    { LOW,  LOW, HIGH, HIGH}
+};
+
+std::sig_atomic_t volatile g_motor_step_interval_done;
+} // end anonymous namespace
+
+void
+setup()
+{
+  IntervalTimer* step_timer{get_g_step_timer()};
+  step_timer->begin(timer_handler, 800);
+}
+
+void
+timer_handler()
+{
+  // Interrupts are disabled in hardware for this scope.
+  //
+  // (https://pjrc.com/teensy/interrupts.html)
+  g_motor_step_interval_done = 1;
+}
+
+IntervalTimer*
+get_g_step_timer()
+{
+  static IntervalTimer* step_timer{new IntervalTimer};
+  return step_timer;
+}
+
+void
+loop()
+{
+  static int stepper_state{0};
+  // We do not have to disable interrupts here, because the state is of an
+  // atomic type safe for access between signal handlers (it probably disables
+  // interrupts in its implementation).
+  if (g_motor_step_interval_done) {
+    g_motor_step_interval_done = 0;
+    for (int i = 0; i < 4; ++i) {
+      digitalWrite(STEPPER_PINS[i], STATE_ORDER[stepper_state][i]);
+    }
+    ++stepper_state;
+    if (4 == stepper_state) {
+      stepper_state = 0;
+    }
+  }
+}
